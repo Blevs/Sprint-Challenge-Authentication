b394e9334beb9dd54abf0aeaddb4ef145b2f9db3
Henry Blevins <heblevi@gmail.com>
2019-05-10 11:20:23 -0400

    Implement registration

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,6 @@
 const axios = require('axios');
+const bcrypt = require('bcrypt');
+const db = require('../database/dbConfig.js');
 
 const { authenticate } = require('../auth/authenticate');
 
@@ -9,12 +11,26 @@ module.exports = server => {
 };
 
 function register(req, res) {
-  // implement user registration
+  const user = req.body;
+  if (user.username && user.password) {
+    user.password = bcrypt.hashSync(user.password, 10);
+    db('users')
+      .insert(user)
+      .then(([id]) =>
+            db('users').where({id}).first()
+            .then(user => res.status(201).json(user)))
+      .catch(error => res.status(500).json({
+        message: "Error registering user",
+        error
+      }));
+  } else {
+    res.status(400).json({message: "Requires username and password"});
+  }
 }
 
 function login(req, res) {
-  // implement user login
 }
+
 
 function getJokes(req, res) {
   const requestOptions = {
