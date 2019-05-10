846b4d80e00c907ea3af8e4a49b74fe1f6da43ac
Henry Blevins <heblevi@gmail.com>
2019-05-10 12:03:37 -0400

    Implement login and jokes

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import { NavLink, Route } from 'react-router-dom';
 import Register from './views/Register';
+import Login from './views/Login';
+import Jokes from './views/Jokes';
 
 
 function App() {
@@ -13,6 +15,8 @@ function App() {
         <NavLink to='/jokes'>Jokes</NavLink>
       </header>
       <Route path='/register' component={Register}/>
+      <Route path='/login' component={Login}/>
+      <Route path='/jokes' component={Jokes}/>
     </div>
   );
 }
